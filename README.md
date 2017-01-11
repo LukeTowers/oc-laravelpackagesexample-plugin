2b05581f484d8063fd223368d736c537db96dfcc
Luke Towers <luke@luketowers.ca>
2017-01-11 03:06:11 -0600

    Fix config keys with dots in them

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # oc-laravelpackagesexample-plugin
 OctoberCMS plugin for demonstrating the use of Laravel Packages within October plugins
+
+
+## TODO:
+ - Test Config::set($config_namespace, $options['config']); instead of the looping through the keys for simplified code and support for config keys with dots in them
--- config/config.php
+++ config/config.php
@@ -23,18 +23,24 @@
                 'cacheFileMode' => 0755,
                 'settings'      => [
                     'default' => [
-                        'HTML.Doctype'             => 'XHTML 1.0 Strict',
-                        'HTML.Allowed'             => 'div,b,strong,i,em,a[href|title],ul,ol,li,p[style],br,span[style],img[width|height|alt|src]',
-                        'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align',
-                        'AutoFormat.AutoParagraph' => true,
-                        'AutoFormat.RemoveEmpty'   => true,
+                        'HTML' => [
+                            'Doctype'             => 'XHTML 1.0 Strict',
+                            'Allowed'             => 'div,b,strong,i,em,a[href|title],ul,ol,li,p[style],br,span[style],img[width|height|alt|src]',
+                        ],
+                        'CSS'  => [
+                            'AllowedProperties'   => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align',
+                        ],
+                        'AutoFormat' => [
+                            'AutoParagraph' => true,
+                            'RemoveEmpty'   => true,
+                        ],
                     ],
                     'test'    => [
-                        'Attr.EnableID' => true
+                        'Attr' => ['EnableID' => true]
                     ],
                     "youtube" => [
-                        "HTML.SafeIframe"      => 'true',
-                        "URI.SafeIframeRegexp" => "%^(http://|https://|//)(www.youtube.com/embed/|player.vimeo.com/video/)%",
+                        "HTML" => ["SafeIframe" => 'true'],
+                        "URI"  => ["SafeIframeRegexp" => "%^(http://|https://|//)(www.youtube.com/embed/|player.vimeo.com/video/)%"],
                     ],
                 ],
             ],
